927e8880b2c245b070daa6fc76656b1346ca013c
evwhiz <evwhiz@gmail.com>
2018-09-03 16:21:18 -0400

    GET tasks endpoint

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,24 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 from app import routes
+
+tasks = [
+    {
+        "id": 1,
+        "title": u"Buy groceries",
+        "description": u"Milk, Cheese, Pizza, Fruit, Tylenol",
+        "done": False
+    },
+    {
+        "id": 2,
+        "title": u"Learn Python",
+        "description": u"Need to find a good Python tutorial on the web",
+        "done": False
+    }
+]
+
+@app.route("/todo/api/v1.0/tasks", methods=["GET"])
+def get_tasks():
+    return jsonify({"tasks": tasks})
